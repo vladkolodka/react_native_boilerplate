8badfd901e3339dd8f8698501f4b3e10947cefe4
Vladyslav <vladkolodka@gmail.com>
2017-11-15 13:56:40 +0200

    Removed redux-persist

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,24 +1,16 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import { autoRehydrate, persistStore } from 'redux-persist';
-import { AsyncStorage } from 'react-native';
-import immutableTransform from 'redux-persist-transform-immutable';
 
 import rootReducer from './reducers';
 
 export function configureStore(initialState, persistDataLoadedCallback) {
     const store = createStore(rootReducer, initialState, compose(
-        autoRehydrate()
     ));
 
     if (module.hot) module.hot.accept(() => {
         store.replaceReducer(require('./reducers').default);
     });
 
-    persistStore(store, {
-        storage: AsyncStorage,
-        whitelist: ['auth', 'journals'],
-        transforms: [immutableTransform()]
-    }, persistDataLoadedCallback)/* .purge() */;
+    setTimeout(persistDataLoadedCallback, 1);
 
     return store;
 }
